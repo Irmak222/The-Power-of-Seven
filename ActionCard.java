5ffbc24210ef6e7f427a03f76a69e3bf4d78c02c
Irmak <irmak.ceylan@ug.bilkent.edu.tr>
2026-07-08 15:28:34 +0300

    ActionCard.java
    Updated three attributes' visibility.

--- ActionCard.java
+++ ActionCard.java
@@ -1,11 +1,11 @@
 // Represents action cards that can change game state
 public class ActionCard extends Card {
     // Freeze card: Forces a target player to stop flipping and lock current points.
-    private static final String FREEZE = "FREEZE";
+    public static final String FREEZE = "FREEZE";
     // Flip Three card: Forces a target to draw 3 consecutive cards.
-    private static final String FLIP_THREE = "FLIP_THREE";
+    public static final String FLIP_THREE = "FLIP_THREE";
     // Second Chance card: Discarded to prevent a bust on duplicate cards.
-    private static final String SECOND_CHANCE = "SECOND_CHANCE";
+    public static final String SECOND_CHANCE = "SECOND_CHANCE";
     // holds action type for a specific card
     private final String actionType;
 
