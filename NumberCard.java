7af9a41eeaaded1ab2ba72ce49d868804d54dbfe
Irmak <irmak.ceylan@ug.bilkent.edu.tr>
2026-07-08 10:13:02 +0300

    NumberCard.java

--- NumberCard.java
+++ NumberCard.java
@@ -1,3 +1,15 @@
+// Represents the numeric cards in the game from 0 to 12.
 public class NumberCard extends Card {
-    
+    private final int value;  // the numeric value of the card
+
+    // Constructor for NumberCard
+    public NumberCard(String id, String cardName, int value) {
+        super(id, cardName);
+        this.value = value;
+    }
+
+    // getter for the value of the card
+    public int getValue() {
+        return value;
+    }
 }
