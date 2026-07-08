8d9407bfb96bcb38ac63e64c1444117ea0ea57f9
Irmak <irmak.ceylan@ug.bilkent.edu.tr>
2026-07-08 10:13:25 +0300

    ModifierCard.java

--- ModifierCard.java
+++ ModifierCard.java
@@ -1,3 +1,23 @@
+// Represents score modifier cards
 public class ModifierCard extends Card {
+    private final int bonusPoints; // bonus points to add (0 if it is a multiplier card)
+    private final boolean isMultiplier; // flag to determine if this card multiplies the score instead of adding bonus points
+
+    // Constructor for ModifierCard
+    public ModifierCard(String id, String cardName, int bonusPoints, boolean isMultiplier) {
+        super(id, cardName);
+        this.bonusPoints = bonusPoints;
+        this.isMultiplier = isMultiplier;
+    }
+
+    // getter for bonus points
+    public int getBonusPoints() {
+        return bonusPoints;
+    }
+
+    // getter to check if it is a multiplier card
+    public boolean isMultiplier() {
+        return isMultiplier;
+    }
     
 }
