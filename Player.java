3af14fe90789525e627cd11d53d575b48d7b0ecd
Irmak <irmak.ceylan@ug.bilkent.edu.tr>
2026-07-08 10:14:00 +0300

    Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,6 @@
+// Represents a user session in the local gameplay.
+// Manages the player's identification, dynamic score calculations, current hand status
+// and specific state flags (e.g. busted, frozen).
 public class Player {
     
 }
