60d60269a3b77765ea1181e4bbec1f2d16ace161
Irmak <irmak.ceylan@ug.bilkent.edu.tr>
2026-07-12 13:07:15 +0300

    GameManager.java
    Attributes and methods are added but methods are not completed yet.

--- GameManager.java
+++ GameManager.java
@@ -1,3 +1,54 @@
+import java.util.ArrayList;
+
 public class GameManager {
-    
+    // A card deck for the game
+    private final Deck deck;
+    // Players
+    private final ArrayList<Player> players;
+    // Tracks which player's turn
+    private int currentPlayerIndex;
+    // Tracks the which rouns is being played currently
+    private int currentRound;
+    // The target score that players should reach to win the game
+    private static final int TARGET_SCORE = 200;
+
+    // Constructor
+    public GameManager() {
+        this.deck = new Deck(); // initialize a new deck
+        this.players = new ArrayList<>();
+        this.currentPlayerIndex = 0;
+        this.currentRound = 1;
+    }
+
+    // Starting a new game 
+    public void startNewGame(ArrayList<Player> activePlayers) {
+
+    }
+
+    // Starting a new round by shuffling the deck and resets all players' states
+    public void startNewRound() {
+
+    }
+
+    // Executing Flip action (drawing card)
+    public void flip() {
+
+    }
+
+    // Executing Stay action
+    public void stay() {
+
+    }
+
+    // Passes turn to the next active player
+    private void passTurn() {
+
+    }
+
+    // Checks if any player reachs the target score
+    public boolean isGameOver() {
+
+    }
+
 }
+
