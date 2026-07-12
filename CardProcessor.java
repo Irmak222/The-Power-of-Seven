9564138ab4c42a166439c44d61ef610568336f6a
Irmak <irmak.ceylan@ug.bilkent.edu.tr>
2026-07-12 13:06:03 +0300

    Add files via upload
    A new class was created to make managing some methods easier and class organization clearer. Some methods from GameManager and Player were moved to CardProcessor class.

---
+++ CardProcessor.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class CardProcessor {
+    
+    private CardProcessor() {
+
+    }
+
+    // Checks if the player has same card with drawn card
+    public static boolean hasDuplicate(ArrayList<Card> hand, int searchValue) {
+
+    }
+
+    // Checks if the player has Second Chance card
+    public static boolean hasSecondChance(Player player) {
+
+    }
+
+    // Player using Second Chance card to revive
+    public static void useSecondChance(Player player, Card duplicate) {
+
+    }
+
+    // Player playing Freeze card against opponent player
+    public static void playFreeze(Player targetPlayer) {
+
+    }
+
+    // Player flipping three cards consecutively
+    public static void flipThree(Player player, Deck deck) {
+
+    }
+
+    // Ends round with score calculation and clears the table
+    private static int calculateHandScore(ArrayList<Card> hand) {
+
+    }
+
+    // Clears player's hand
+    public static void clearHand(Player player) {
+
+    }
+}
