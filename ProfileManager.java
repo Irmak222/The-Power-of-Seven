cb840401002eb342f18b68e08ddb3e628cbf8c76
B.K. <fbengisuk@gmail.com>
2026-07-13 14:42:38 +0300

    Filled methods other than saveStats

--- ProfileManager.java
+++ ProfileManager.java
@@ -9,16 +9,28 @@ public class ProfileManager {
         this.registeredUsers = new ArrayList<>();
     }
 
-    // True if the registration is successful, false if the username is already taken
+    /// True if the registration is successful, false if the username is already taken
     public boolean register(String userName) {
-
+        for(Player player : registeredUsers){
+            if(player.getName().equals(userName)){
+                return false;
+            }
+        }
+        Player player = new Player(userName, false);
+        registeredUsers.add(player);
+        return true;
     }
 
     // True if the profile exists, false otherwise
     public boolean login(String userName) {
-        
+        for(Player player : registeredUsers){
+            if(player.getName().equals(userName)){
+                return true;
+            }
+        }
+        return false;
     }
-
+    
     // Updates the profile owner's score
     public void saveStats(String userName, int score) {
 
@@ -26,7 +38,7 @@ public class ProfileManager {
 
     // Checks the registry to verify whether there is an account or not
     private boolean isRegistered(String userName) {
-
+        return login(userName);
     }
 
     // Getters
