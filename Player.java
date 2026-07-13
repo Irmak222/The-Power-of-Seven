9c6c3751493bd93d61f89a318d55d445a216c0cb
B.K. <fbengisuk@gmail.com>
2026-07-13 14:29:35 +0300

    Round score added to reset round state

--- Player.java
+++ Player.java
@@ -51,6 +51,7 @@ public class Player {
 
     // Resets the round state after every round
     public void resetRoundState() {
+        addPointsToTotalScore(this.roundScore); // adds round score to total score
         this.activeHand.clear(); // clears player's hand
         this.roundScore = 0; // resets the score
         this.isBusted = false; // player is not busted yet
