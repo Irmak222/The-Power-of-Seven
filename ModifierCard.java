a98905ae057b74416c2fd553d6a13e1e7addd724
B.K. <fbengisuk@gmail.com>
2026-07-13 10:20:32 +0300

    Removed isDivider
    Took back the recent changes I did.

--- ModifierCard.java
+++ ModifierCard.java
@@ -1,8 +1,7 @@
 // Represents score modifier cards
 public class ModifierCard extends Card {
-    private final int bonusPoints; // bonus points to add (0 if it is a multiplier card) or points to be taken away
-    private final boolean isMultiplier; // flag to determine if this card multiplies the score instead of adding bonus points or dividing points.
-    private final boolean isDivider; // flag to determine if this card divides the score instead of adding bonus points or multiplies points.
+    private final int bonusPoints; // bonus points to add (0 if it is a multiplier card)
+    private final boolean isMultiplier; // flag to determine if this card multiplies the score instead of adding bonus points
     
     // Constructor for ModifierCard
     public ModifierCard(String id, String cardName, int bonusPoints, boolean isMultiplier) {
@@ -21,9 +20,4 @@ public class ModifierCard extends Card {
         return isMultiplier;
     }
 
-    // getter to check if it is a divider card
-    public boolean isDivider(){
-        return isDivider;
-    }
-    
 }
