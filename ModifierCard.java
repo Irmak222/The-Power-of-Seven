3ca9fd8cced4cf3d7ca3259048437c3aeb3a76bc
B.K. <fbengisuk@gmail.com>
2026-07-13 10:09:02 +0300

    Divider functionality
    Added isDivider flag and updated bonusPoints and isMultiplier comments. So the points can be taken away via bonusPoints and points can be divided.

--- ModifierCard.java
+++ ModifierCard.java
@@ -1,8 +1,9 @@
 // Represents score modifier cards
 public class ModifierCard extends Card {
-    private final int bonusPoints; // bonus points to add (0 if it is a multiplier card)
-    private final boolean isMultiplier; // flag to determine if this card multiplies the score instead of adding bonus points
-
+    private final int bonusPoints; // bonus points to add (0 if it is a multiplier card) or points to be taken away
+    private final boolean isMultiplier; // flag to determine if this card multiplies the score instead of adding bonus points or dividing points.
+    private final boolean isDivider; // flag to determine if this card divides the score instead of adding bonus points or multiplies points.
+    
     // Constructor for ModifierCard
     public ModifierCard(String id, String cardName, int bonusPoints, boolean isMultiplier) {
         super(id, cardName);
@@ -18,6 +19,11 @@ public class ModifierCard extends Card {
     // getter to check if it is a multiplier card
     public boolean isMultiplier() {
         return isMultiplier;
+    }
+
+    // getter to check if it is a divider card
+    public boolean isDivider(){
+        return isDivider;
     }
     
 }
